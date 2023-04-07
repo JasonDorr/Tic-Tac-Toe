3761d97652e226ff948783317812b1b3cbac404c
Jason Dorr <jasondorr95@gmail.com>
2023-04-07 14:35:40 -0400

    Game logic is finished. Needs styling, game over message, and player turn message.

--- app.js
+++ app.js
@@ -1,26 +1,100 @@
-const Gameboard = (() => {
-  // DOM Elements
+const Player = (sign) => {
+  const playerSign = sign;
 
-  const playArea = document.querySelector("#game-board");
-
-  // Creates the Gameboard
-  const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  const render = () => {
-    board.forEach((cell) => {
-      cell.toString();
-      const newCell = document.createElement("div");
-      newCell.textContent = cell;
-      newCell.className = "board-cell";
-      playArea.appendChild(newCell);
-    });
-  };
-
-  render();
+  const getSign = () => playerSign;
 
   return {
-    board,
+    getSign,
+  };
+};
+
+const gameBoard = (() => {
+  // Creates the Gameboard
+  const board = new Array(9);
+
+  const getField = (index) => board[index];
+
+  const setField = (index, sign) => {
+    board[index] = sign;
+  };
+
+  return {
+    getField,
+    setField,
   };
 })();
 
-console.log(Gameboard.board);
+const displayController = (() => {
+  const fieldCells = Array.from(document.querySelectorAll(".board-cell"));
+
+  const updateBoard = () => {
+    for (let i = 0; i < fieldCells.length; i++) {
+      fieldCells[i].textContent = gameBoard.getField(i);
+    }
+  };
+
+  fieldCells.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      if (gameController.gameOver() || e.target.textContent !== "") return;
+
+      gameController.playRound(parseInt(e.target.dataset.index));
+
+      updateBoard();
+    });
+  });
+})();
+
+const gameController = (() => {
+  const playerX = Player("X");
+  const playerO = Player("O");
+  let round = 1;
+  let isOver = false;
+
+  const getCurrentPlayer = () =>
+    round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+
+  const playRound = (index) => {
+    gameBoard.setField(index, getCurrentPlayer());
+    if (isWinner(index)) {
+      isOver = true;
+      return;
+    }
+
+    if (isDraw(round)) {
+      isOver = true;
+      return;
+    }
+    round++;
+  };
+
+  const isDraw = (roundCount) => roundCount === 9;
+
+  const isWinner = (fieldIndex) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winConditions
+      .filter((combination) => combination.includes(fieldIndex))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (index) => gameBoard.getField(index) === getCurrentPlayer()
+        )
+      );
+  };
+
+  const gameOver = () => isOver;
+
+  return {
+    getCurrentPlayer,
+    playRound,
+    gameOver,
+  };
+})();
